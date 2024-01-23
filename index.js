51f39ec82bbea444028c487638c4ae85cbaa1a7c
Daniel Westergren <carl.dawe@gmail.com>
2024-01-23 10:39:42 +0100

    made it in 10 rows, fully automated system for everythin!

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-function add(child, parent, text, appendstyle) {
-  child = document.createElement(child);
-  parent = document.querySelector(parent);
-  child.innerText = text;
+const P = (...txt) => txt.map((t, p) => ((p = document.createElement("p")), (p.innerText = t), p)),
+  list = document.querySelector("section"),
+  add = (p, ...app) => p.forEach((c, i) => (app[0] ? list.insertAdjacentElement(app[i] || app[0], c) : list.appendChild(c))),
+  inner = (...txt) => txt.map((t) => (list.innerHTML += `<p>${t}</p>`)) && console.log(list),
+  rep = (t, txt, c, d = document.querySelector(t)) => (c) => c >= 0 ? d.replaceChild(txt[0], d.children[c]) : (d.innerText = txt);
 
-  appendstyle ? parent.insertAdjacentElement(appendstyle, child) : parent.appendChild(child);
-}
-
-p1 = add("p", "section", "Paragraph: appendChild", "");
-p2 = add("p", "section", "Paragrap: insertAdjacentElement", "beforeend");
-
-let allP = document.querySelector(".list");
-
-// allP.innerHTML = "<p>Added with innerHTML, WHERE DID MY LAST TEXT GO!?</p>"; <- REMOVES THE EARLIER DATA AND ADD THIS STRING INSTEAD IN HTML DOC
-
-allP.innerHTML += "<p>Added with innerHTML</p>"; // <- This adds the new data with the old data in doc.
-
-["LOOPED Text 1", "LOOPED Text 2", "LOOPED Text 3"].map((txt) => (allP.innerHTML += `<p>${txt}</p>`)); // Neat little mapping on the array' ye!
-
-console.log(allP.children.length); // CONSOLE LOGS number of children in bucketlist
-
-const newname = document.querySelector("h2");
-newname.innerText = "Daniel Westergrens";
-
-newchild = document.createElement("b");
-newchild.innerText = "Im the new child on top!";
-oldchild = allP.children[0];
-allP.replaceChild(newchild, oldchild);
-
-newchild = document.createElement("b");
-newchild.innerText = "IM TOTALLY MIDDLECLASS";
-oldchild = allP.children[~~(allP.children.length / 2)]; // The ol' ~~ cute lil trick. Math ftw. If postive int's. ^^
-allP.replaceChild(newchild, oldchild);
-
-allP.removeChild(allP.lastChild); // Byebye mr Last P guy!
+add(P("Earn another million dollars.")), add(P("Move to a exotic place."), "beforeend"), inner("Learn cooking better.");
+add(P("Find a meaningful job.", "Travel to majority of all countries on earth.", "Take driver license!"));
+console.log(`${list.nodeName} contains ${list.childElementCount} elements...`), rep("h2", "Daniels"), rep(list.nodeName, 
+P("Find happiness."), 1), rep(list.nodeName, P("Learn painting and cooking."), ~~(list.childElementCount / 2)), list.removeChild(list.lastChild);
